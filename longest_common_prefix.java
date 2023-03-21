0f365d90f5baae064ca13fbbec124b0e79857c43
Avanthi-aj <84899743+Avanthi-aj@users.noreply.github.com>
2023-03-21 18:14:59 +0530

    Update longest_common_prefix.java

--- longest_common_prefix.java
+++ longest_common_prefix.java
@@ -1,8 +1,6 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
        Arrays.sort(strs);
-       int fst=strs[0].length();
-       int lst=strs[strs.length-1].length();
        int count=0;
       for(int i=0;i<strs[0].length();i++)
        {
@@ -15,7 +13,6 @@ class Solution {
                break;
            }
        }
-
         return strs[0].substring(0,count);
     }
 }
